f8eecf07c7257c19743117c135a5afc4d9f489b7
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2024-05-13 11:18:34 +0200

    Script for building, running and gracefully shutting down the program

---
+++ run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Check if all arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <device_name> <ip_address>/<network_mask>"
+    echo "Example: $0 tun0 192.168.10.1/24"
+    exit 1
+fi
+
+# Declare variables
+device_name="$1"
+ip_address="$2"
+
+# Build the program
+zig build
+
+# Attach network administration capability to compiled executable
+sudo setcap CAP_NET_ADMIN=ep ./zig-out/bin/main
+
+# Run an executable in the background
+./zig-out/bin/main $device_name &
+
+# Write the process id into $pid variable
+pid=$!
+
+# Assign IPv4 address to the TUN interface
+sudo ip addr add $ip_address dev $device_name
+
+# Bring the TUN link up
+sudo ip link set up dev $device_name
+
+# Clean up function for graceful shutdown
+cleanup() {
+    # Bring the TUN link down
+    sudo ip link set down dev $device_name
+
+    # Remove the assigned IPv4 address from the TUN interface
+    sudo ip addr del $ip_address dev $device_name
+
+	# Kill the process
+	kill $pid
+
+    exit
+}
+#
+# Run "cleanup" function when SIGINT or SIGTERM is received
+trap cleanup SIGINT SIGTERM
+
+# Wait for the program to finish
+wait $pid
