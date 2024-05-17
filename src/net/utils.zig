504e8c6c4494c15acb23512007b7dd1013e766a8
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2024-05-17 14:02:08 +0200

    Unit tests for the net/utils functions

--- src/net/utils.zig
+++ src/net/utils.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub fn intFromBytes(comptime T: type, slice: []const u8) T {
     var sum: T = 0;
     for (0.., slice) |i, el| {
@@ -5,4 +7,11 @@ pub fn intFromBytes(comptime T: type, slice: []const u8) T {
         sum += @as(T, el) << @intCast(slf);
     }
     return sum;
+}
+
+test "integer from slice of bytes" {
+    try std.testing.expectEqual(intFromBytes(u8, &.{0x1}), @as(u8, 0x01));
+    try std.testing.expectEqual(intFromBytes(u16, &.{ 0x2, 0x1 }), @as(u16, 0x0201));
+    try std.testing.expectEqual(intFromBytes(u32, &.{ 0x3, 0x2, 0x1 }), @as(u32, 0x030201));
+    try std.testing.expectEqual(intFromBytes(u8, &.{}), @as(u8, 0x0));
 }
